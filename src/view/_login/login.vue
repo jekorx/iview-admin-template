1c49dfcbcddbd7a3dcadf3af1926690f2f71e11f
wang_dgang <wdg404@163.com>
2019-03-13 19:05:06 +0800

    login background

--- src/view/_login/login.vue
+++ src/view/_login/login.vue
@@ -7,14 +7,49 @@
         </div>
       </Card>
     </div>
+    <div class="wave" ref="waveRef"></div>
   </div>
 </template>
 <script>
 import LoginForm from '_c/login-form'
 import { mapActions } from 'vuex'
+import ParticleWave from 'particle-wave'
+
 export default {
   components: {
     LoginForm
+  },
+  mounted () {
+    const pointSize = 4
+    this.$nextTick(() => {
+      /* eslint-disable */
+      new ParticleWave(this.$refs.waveRef, {
+        uniforms: {
+          size: { type: 'float', value: pointSize },
+          field: { type: 'vec3', value: [ 0, 0, 0 ] },
+          speed: { type: 'float', value: 7 }
+        },
+        onResize (w, h, dpi) {
+          const position = []
+          const color = []
+          const width = 400 * (w / h)
+          const depth = 500
+          const height = 7
+          const distance = 9
+          for (let x = 0; x < width; x += distance) {
+            for (let z = 0; z < depth; z += distance) {
+              position.push(-width / 2 + x, -30, -depth / 2 + z)
+              color.push(0, 1 - (x / width) * 1, 0.5 + x / width * 0.5, z / depth)
+            }
+          }
+          this.uniforms.field = [ width, height, depth ]
+          this.buffers.position = position
+          this.buffers.color = color
+          this.uniforms.size = (h / 400) * pointSize * dpi
+        }
+      })
+      /* eslint-enable */
+    })
   },
   methods: {
     ...mapActions([
